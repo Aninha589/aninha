8ef5f55eb677c12ea47df99a6b8133f4068e0b53
Aninha589 <clara.nascimento.ana@escola.pr.gov.br>
2024-09-06 16:08:52 +0000

    ana

--- sketch.js
+++ sketch.js
@@ -4,22 +4,22 @@ let px;
 let py;
 function setup() {
   createCanvas(600, 450);
-  background("white");  
-  px = (200);
-  py = (200)
-}
+    background("white");  
+      px = (200);
+        py = (200)
+        }
 
-function draw() {
-  tx = (random(0, 50))
-  ty = (random(0, 50))
-  cor = color(random(0, 255), random(0, 255), random(0, 255));
- 
-  if(mouseIsPressed){
-    fill(cor);
-    circle(px, py, 30)
-   
-      px = mouseX
-     py = mouseY
-  }
-    
-}
+        function draw() {
+          tx = (random(0, 50))
+            ty = (random(0, 50))
+              cor = color(random(0, 255), random(0, 255), random(0, 255));
+               
+                 if(mouseIsPressed){
+                     fill(cor);
+                         circle(px, py, 30)
+                            
+                                  px = mouseX
+                                       py = mouseY
+                                         }
+                                             
+                                             }
